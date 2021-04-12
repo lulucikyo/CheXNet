573236a973f7b1e45d00c658c898cdaed91ea08e
lulucikyo <lixi220@outlook.com>
2021-04-12 16:18:40 -0400

    Test: local run

--- replication_chexnet_cuda_local.py
+++ replication_chexnet_cuda_local.py
@@ -7,14 +7,20 @@ Original file is located at
     https://colab.research.google.com/drive/1-S1WhqiqsChuWVGjunT_5CIGdlV-kHO3
 """
 
-#from google.colab import drive
-#drive.mount('/content/drive')
+# Commented out for local run
+"""
+from google.colab import drive
+drive.mount('/content/drive')
+"""
 
 """You want to use the full path for Colab"""
 
 # Commented out IPython magic to ensure Python compatibility.
 # %cd /content/drive/My Drive/DL4H Project/replication/images_converted/
-#!(ls | wc -l)
+# Commented out for local run
+"""
+!(ls | wc -l)
+"""
 # %cd /content/drive/My Drive/DL4H Project/replication
 
 """Yes, there is some dups in the dataset, but it does not affect"""
@@ -36,32 +42,20 @@ from sklearn.metrics import roc_auc_score
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
-# import zipfile
 
-# # extract_path = "./image_converted/"
-# filename = "images_converted.zip"
-# print(filename)
-# zip = zipfile.ZipFile(filename)
-# zip.extractall(".")
 
-# CUDA stats
-print(torch.cuda.device_count())
-print(torch.cuda.get_device_name(0))
-
-N_CLASSES = 14
-CLASS_NAMES = ["Atelectasis","Cardiomegaly", "Effusion", "Infiltration", "Mass", 
+N_LABEL = 14
+LABELS = ["Atelectasis","Cardiomegaly", "Effusion", "Infiltration", "Mass", 
                "Nodule", "Pneumonia", "Pneumothorax", "Consolidation", "Edema", 
                "Emphysema", "Fibrosis", "Pleural_Thickening", "Hernia"]
-DATA_DIR = './images_converted/'
-#DATA_DIR = '/content/drive/My Drive/DL4H Project/replication/images_converted/'
-TEST_IMAGE_LIST = 'labeled_test_list.txt'
+DATA_PATH = './images_converted/'
+#DATA_PATH = '/content/drive/My Drive/DL4H Project/replication/images_converted/'
 BATCH_SIZE = 16
+N_EPOCH = 2
 PRINT_INTERVAL = 50
-
 """BATCH_SIZE -> 8 is way better than 16 in Colab"""
 
-# img_list_ = os.listdir("./images_converted/")
-# print(len(img_list_))
+
 
 def collate_fn(data):
     image_path, label = zip(*data)
@@ -73,12 +67,8 @@ def collate_fn(data):
                 ])
     for img in image_path:
         img_pil = Image.open(img).convert("RGB")
-        #print(img_pil)
         img_tensor = trans(img_pil).unsqueeze(0)
-        #print(img_tensor)
         image_tensors = torch.cat((image_tensors, img_tensor))
-
-
     label_tensors = torch.FloatTensor(label)
 
     return image_tensors.cuda(), label_tensors.cuda()
@@ -100,12 +90,15 @@ class XrayDataSet(Dataset):
         return(self.image_path[index], self.y[index])
 
 class DenseNet121(nn.Module):
-    def __init__(self, out_size):
+    """
+    The last layer of DenseNet121 was replaced by a Linear with 14 output features, followed by a sigmoid function
+    """
+    def __init__(self, out_feature):
         super(DenseNet121, self).__init__()
         self.densenet121 = torchvision.models.densenet121(pretrained=True)
-        num_ftrs = self.densenet121.classifier.in_features
+        in_features = self.densenet121.classifier.in_features
         self.densenet121.classifier = nn.Sequential(
-            nn.Linear(num_ftrs, out_size),
+            nn.Linear(in_features, out_feature),
             nn.Sigmoid()
         )
 
@@ -116,7 +109,7 @@ class DenseNet121(nn.Module):
 def train_model(model, train_loader, n_epochs = 1):
     t1 = time.time()
     criterion = nn.BCELoss()
-    """using Adam with standard parameters (B1 = 0:9 and B2 = 0:999) """
+    """using Adam with standard parameters (B1 = 0.9 and B2 = 0.999) """
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     """factor (float) – Factor by which the learning rate will be reduced. new_lr = lr * factor. Default: 0.1."""
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1,
@@ -134,12 +127,10 @@ def train_model(model, train_loader, n_epochs = 1):
         torch.cuda.empty_cache()
         train_loss = 0
         batch = 0
-        print("Started epoch {}".format(epoch))
+        print("Started epoch {}".format(epoch+1))
         for x, y in train_loader:
             optimizer.zero_grad()
             y_hat = model(x)
-            #print(y.shape)
-            #print(y_hat.shape)
             loss = criterion(y_hat, y)
             loss.backward()
             optimizer.step()
@@ -150,9 +141,9 @@ def train_model(model, train_loader, n_epochs = 1):
 
         train_loss = train_loss / len(train_loader)
         scheduler.step(train_loss)
-        if epoch % 1 == 0:
-            train_loss_arr.append(np.mean(train_loss))
-            print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch+1, train_loss))
+        
+        train_loss_arr.append(np.mean(train_loss))
+        print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch+1, train_loss))
 
     t2 = time.time()
     print("Training time lapse: {} min".format((t2 - t1) // 60))
@@ -165,86 +156,67 @@ def eval_model(model, test_loader):
     y_pred = y_pred.cuda()
     print("Evaluating test data...\t test_loader: {}".format(len(test_loader)))
     t1 = time.time()
-    for i, (inp, target) in enumerate(test_loader):
-        target = target.cuda()
-        y_test = torch.cat((y_test, target), 0)
-        bs, c, h, w = inp.size()
-        # input_var = torch.autograd.Variable(inp.view(-1, c, h, w).cuda(), volatile=True)
+    for i, (x, y) in enumerate(test_loader):
+        y = y.cuda()
+        y_test = torch.cat((y_test, y), 0)
+        batch_size, channel, hight, width= x.size()
         with torch.no_grad():
-            input_var = torch.autograd.Variable(inp.view(-1, c, h, w).cuda())
-        output = model(input_var)
-        # output_mean = output.view(bs, -1).mean(1)
-        y_pred = torch.cat((y_pred, output), 0)
-        if (i % 10 == 0):
+            x_in = torch.autograd.Variable(x.view(-1, channel, height, width).cuda())
+        y_hat = model(x_in)
+        y_pred = torch.cat((y_pred, y_hat), 0)
+        if (i % PRINT_INTERVAL == 0):
             print("batch: {}".format(i))
     t2 = time.time()
     print("Evaluating time lapse: {} min".format((t2 - t1) // 60))
     
-    AUROCs = compute_AUCs(y_test, y_pred)
-    # print(AUROCs)
-    # print(len(AUROCs))
-    AUROC_avg = np.array(AUROCs).mean()
-    print('The average AUROC is {AUROC_avg:.3f}'.format(AUROC_avg=AUROC_avg))
-    for i in range(N_CLASSES):
-        print('The AUROC of {} is {}'.format(CLASS_NAMES[i], AUROCs[i]))
-
-def compute_AUCs(y_test, y_pred):
-    """Computes Area Under the Curve (AUC) from prediction scores.
-    Args:
-        y_test: Pytorch tensor on GPU, shape = [n_samples, n_classes]
-          true binary labels.
-        y_pred: Pytorch tensor on GPU, shape = [n_samples, n_classes]
-          can either be probability estimates of the positive class,
-          confidence values, or binary decisions.
-    Returns:
-        List of AUROCs of all classes.
-    """
+    """Compute AUROC for each class"""
     AUROCs = []
     y_test_np = y_test.cpu().detach().numpy()
     y_pred_np = y_pred.cpu().detach().numpy()
-    # y_pred_np = np.transpose([pred[:, 1] for pred in y_pred_np])
-    # print(y_pred_np.shape)
-    for i in range(N_CLASSES):
-        # print(y_test_np[:, i])
-        # print(y_pred_np[:, i])
+    for i in range(N_LABEL):
         result = roc_auc_score(y_test_np[:, i], y_pred_np[:, i])
         AUROCs.append(result)
-    return AUROCs
+
+    AUROC_avg = np.array(AUROCs).mean()
+    print('The average AUROC is {AUROC_avg:.3f}'.format(AUROC_avg=AUROC_avg))
+    for i in range(N_LABEL):
+        print('The AUROC of {} is {}'.format(LABELS[i], AUROCs[i]))
+
+
+
+
 
 """Now, let's run"""
+
+# CUDA stats
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0))
 
 # cudnn will look for the optimal set of algorithms for that particular configuration (which takes some time). 
 # This usually leads to faster runtime.
 cudnn.benchmark = True
 
 # initialize and load the model
-model = DenseNet121(N_CLASSES).cuda()
+model = DenseNet121(N_LABEL).cuda()
 
-"""    if False and os.path.isfile(CKPT_PATH):
-  print("=> loading checkpoint")
-  checkpoint = torch.load(CKPT_PATH)
-  model.load_state_dict(checkpoint['state_dict'])
-  print("=> loaded checkpoint")
-else:
-  print("=> no checkpoint found")
+# Small sample for debug purpose. Commented out for full training
+"""
+train_dataset = XrayDataSet(DATA_PATH, "train_val_sample10k.txt")
+test_dataset = XrayDataSet(DATA_PATH, "test_sample1k.txt")
 """
 
-#train_dataset = XrayDataSet(DATA_DIR, "train_val_sample10k.txt")
-#test_dataset = XrayDataSet(DATA_DIR, "test_sample1k.txt")
-
-train_dataset = XrayDataSet(DATA_DIR, "labeled_train_val_list.txt")
-test_dataset = XrayDataSet(DATA_DIR, "labeled_test_list.txt")
+train_dataset = XrayDataSet(DATA_PATH, "labeled_train_val_list.txt")
+test_dataset = XrayDataSet(DATA_PATH, "labeled_test_list.txt")
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
 test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_fn)
 
-train_model(model, train_loader, n_epochs=1)
+train_model(model, train_loader, n_epochs=N_EPOCH)
 
 """No need to use GPU for calculating AUC"""
-
 gc.collect()
 torch.cuda.empty_cache()
 # switch to evaluate mode
 model.eval()
 with torch.no_grad():
-  eval_model(model, test_loader)
+    eval_model(model, test_loader)
